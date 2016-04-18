a703f9d209615ddbf81e8314d84ada52d33c7485
Adam Wilson <adamwilson1234@gmail.com>
2016-04-18 17:50:06 -0500

    added separate controller methods and files exported as modules and required in router. created routes with a view and controller method for each. all work. started jade-bootstrap work with nav and footer elements. nav collapse not working right.

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,16 @@
+/* NO REQUIRES, JUST EXPORTS */
+
+/* GET home page */
+module.exports.homelist = function(req, res) {
+    res.render('index', { title: 'Home' });
+};
+
+/* GET locations info page */
+module.exports.locationInfo = function(req, res) {
+    res.render('index', { title: 'Location Info' });
+};
+
+/* GET add review page */
+module.exports.addReview = function(req, res) {
+    res.render('index', { title: 'Add Review' });
+};
