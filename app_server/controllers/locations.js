4709347819ea09ecad5ae95b6f3eccc60ce93fae
Adam Wilson <adamwilson1234@gmail.com>
2016-04-20 14:35:21 -0500

    moving data out of views into js object in the controller render methods. Using jade variables for data interpolation and buffered js code with delimiters = and #{}.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,29 @@
 /* NO REQUIRES, JUST EXPORTS */
 
 /* GET home page */
+/* RENDER takes view, data object */
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', { title: 'Home' });
-};
+    res.render('locations-list', {
+      title: 'Loc8r - Find a place to work with wifi',
+      pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find a place to work with wifi near you'
+      },
+      locations: [{
+        name: '1212 Lounge',
+        address: '1212 S. Michigan, Chicago, IL 60605',
+        rating: 4,
+        facilities: ['Standing desks', 'Wifi'],
+        distance: '100m'
+      },{
+        name: 'Overflow Coffee Bar',
+        address: '1550 S. State, Chicago, IL 60605',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Locally owned', 'Wifi'],
+        distance: '100m'
+      }]
+    }); // end render
+}; // end homelist
 
 /* GET locations info page */
 module.exports.locationInfo = function(req, res) {
