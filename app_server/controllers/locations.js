48b2796664670b6a558ffd04ba4f5afa915092ad
Adam Wilson <adamwilson1234@gmail.com>
2016-04-21 17:04:11 -0500

    finished moving data out of static views and into controllers as js objects. Views are now decoupled from the data ie data agnostic. everything works.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -9,6 +9,7 @@ module.exports.homelist = function(req, res) {
         title: 'Loc8r',
         strapline: 'Find a place to work with wifi near you'
       },
+      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffe, cake or a pint? Let Loc8r help you find the place you're looking for.",
       locations: [{
         name: '1212 Lounge',
         address: '1212 S. Michigan, Chicago, IL 60605',
@@ -27,10 +28,50 @@ module.exports.homelist = function(req, res) {
 
 /* GET locations info page */
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location Info' });
-};
+    res.render('location-info', {
+      title: '1212 Lounge',
+      pageHeader: {title: '1212 Lounge'},
+      sidebar: {
+        context: "1212 Lounge is on Loc8r because our resident lounge gets the author out of the house for some chill, quiet work space right inside the building. Can't beat that!",
+        callToAction: 'Been there? Like it? or not? leave a review!'
+      },
+      location: {
+        name: '1212 Lounge',
+        address: '1212 S. Michigan, Chicago, IL 60605',
+        rating: 5,
+        facilities: ['Standing desks', 'Wifi'],
+        coords: {lat: 41.867449, lng: -87.624110},
+        openingTimes: [{
+          days: 'Monday-Friday',
+          opening: '7am',
+          closing: '10pm',
+          closed: false
+        }, {
+          days: 'Saturday-Sunday',
+          opening: '7am',
+          closing: '10pm',
+          closed: false
+        }],
+        reviews: [{
+          author: 'Adam Wilson',
+          rating: 5,
+          timeStamp: '21 April 2016',
+          reviewText: 'couches and stand up counter nice variety'
+        },{
+          author: 'Moshe Ben Yamin',
+          rating: 4,
+          timeStamp: '19 April 2016',
+          reviewText: 'nice and quiet!!!'
+        }]
+      }
+    }); // end data object and render arguments
+}; // end locationInfo
+
 
 /* GET add review page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Add Review' });
+    res.render('location-review-form', {
+      title: 'Review 1212 Lounge on Loc8r',
+      pageHeader: {title: 'Review 1212 Lounge'}
+    });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,10 @@
 // export the index method
 module.exports.about = function(req, res) {
     // render (view, data)
-    res.render('generic-text', {title: 'About'} );
+    // note \n newline code in content
+    // we don't want html dirtying up the data
+    res.render('generic-text', {
+      title: "About Loc8r",
+      content:  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    });
 };
